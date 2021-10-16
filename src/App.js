ada16feb4bfdba8a78a619d4f117a6cf47f47315
Nathan Watkins <nathancwatkins23@gmail.com>
2021-10-16 14:49:58 -0400

    initial commmit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React, { useState } from 'react'
+const Header = (prop) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>{prop.text}</h1>
+    </>
+  )
 }
 
-export default App;
+const Button = (prop) => {
+  return (
+    <>
+      <button onClick={prop.increment}>{prop.text}</button>
+    </>
+  )
+}
+const Stats = (prop) => {
+  return (
+    <>
+      <div>
+        {prop.text}: {prop.count}
+      </div>
+    </>
+  )
+}
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <>
+      <Header text='give feedback' />
+      <Button increment={() => setGood(good + 1)} text='good' />
+      <Button increment={() => setNeutral(neutral + 1)} text='neutral' />
+      <Button increment={() => setBad(bad + 1)} text='bad' />
+      <Header text='statistics' />
+      <Stats count={good} text='good' />
+      <Stats count={neutral} text='neutral' />
+      <Stats count={bad} text='bad' />
+    </>
+  )
+}
+
+export default App
